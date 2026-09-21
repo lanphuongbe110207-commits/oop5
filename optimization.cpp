6b6e95372745e9eb8739ca20278fe881ee02b68c
phuongbety <fv25tc557r@privaterelay.appleid.com>
2026-09-21 17:09:05 +0700

    First commit

---
+++ optimization.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class overall{
+private: 
+    int CPU;
+    int time;
+    double ram;
+    double data_trans;
+    float price;
+public: 
+class AWS{
+private:
+    string service;
+    float price;
+    int time;
+public: 
+
+
+
+
+};
+
+
+
+};
+int main(){}
